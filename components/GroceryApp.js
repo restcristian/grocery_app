377aa03a11f2a9f53cb3041355ca5c7f1c636eec
Cristian Restituyo <cristian.restituyo@sdq.theoreminc.net>
2017-03-13 17:37:30 -0400

    Added respective data to establish a connection with firebase backend. In addition, added cross-platform Alert.

--- components/GroceryApp.js
+++ components/GroceryApp.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, Text, ListView} from 'react-native';
+import {View, StyleSheet, Text, ListView,Alert} from 'react-native';
 import * as firebase from 'firebase';
 import StatusBar from './StatusBar';
 import ActionButton from './ActionButton';
 import ListItem from './ListItem';
 
 const firebaseConfig = {
-    apiKey:'',
-    authDomain:'',
-    databaseURL:'',
+    apiKey:' AIzaSyD2nNpD1dbvnyw_z9G0nj354MAibqqRM04',
+    authDomain:'grocery-607a9.firebaseapp.com',
+    databaseURL:'https://grocery-607a9.firebaseio.com/',
     storageBucket:''
 }
 
@@ -24,8 +24,48 @@ export default class GroceryApp extends Component{
                 rowHasChanged: (row1,row2)=>row1 !== row2,
             })
         };
+
+        this.itemsRef = firebaseApp.database().ref();
     }
 
+    _addItem(){
+        Alert.alert(
+            'Add a New Item',
+            null,
+            [
+                {
+                    text:'OK', 
+                    onPress:(text)=>{
+                        console.log('Ok Pressed');
+                        this.itemsRef.push({title:text});
+                      },
+                },
+                {
+                    text:'Cancel',
+                    onPress:()=>console.log('Cancel Pressed'),
+                    style:'cancel'
+                }
+            ],
+            {cancelable:false}
+        )
+    }
+
+    listenForItems(itemsRef){
+        itemsRef.on('value',(snap)=>{
+            let items = [];
+            snap.forEach((child)=>{
+                items.push({
+                    title:child.val().title,
+                    _key:child.key
+                });
+            });
+
+            this.setState({
+                dataSource:this.state.dataSource.cloneWithRows(items)
+            });
+            console.log('my datasource:',this.state.dataSource);
+        });
+    }
     _renderItem(item){
         return(
             <ListItem item = {item} onPress = {()=>{}} />
@@ -40,15 +80,16 @@ export default class GroceryApp extends Component{
                         renderRow = {this._renderItem.bind(this)}
                         style = {styles.listview}
                         />
-                        <ActionButton title = "Add" onPress = {()=>{}} />
+                        <ActionButton title = "Add" onPress = {this._addItem.bind(this)} />
                  </View>
         )
     }
 
     componentDidMount(){
-        this.setState({
-            dataSource:this.state.dataSource.cloneWithRows([{title:'Pizza'}])
-        });
+        // this.setState({
+        //     dataSource:this.state.dataSource.cloneWithRows([{title:'Pizza'}])
+        // });
+        this.listenForItems(this.itemsRef);
     }
 }
 
